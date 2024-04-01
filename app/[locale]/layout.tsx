848ee2382aa24eb48ed30e619ece2804ec709f41
DESKTOP-FMD3QJU\Ira <kyptdev@gmail.com>
2024-04-02 01:29:39 +0700

    Add cookie consent

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar"
 import Footer from "@/components/Footer";
 import { NextIntlClientProvider, useMessages } from 'next-intl'
+import CookieConsent from "@/components/CookiesConsent.tsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +27,7 @@ export default function RootLayout({
         <NextIntlClientProvider messages={messages}>
           <Navbar />
           {children}
+          <CookieConsent />
           <Footer />
         </NextIntlClientProvider>
       </body>

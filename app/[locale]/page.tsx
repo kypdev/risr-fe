0a936e76b75294b23053b5ec9cb458443a32c531
DESKTOP-FMD3QJU\Ira <kyptdev@gmail.com>
2024-04-11 08:07:20 +0700

    add page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,9 +2,10 @@
 import RecentNews from '@/components/RecentNews'
 import Image from "next/image"
 import styles from './style.module.css'
+import { data } from './mockData'
 
 
-const AnyReactComponent = ({ text }: any) => <div>{text}</div>;
+const AnyReactComponent = ({ text }: any) => <div>{text}</div>
 
 export default function Home() {
   const defaultProps = {
@@ -13,23 +14,46 @@ export default function Home() {
       lng: 77.01502627
     },
     zoom: 11
-  };
+  }
   return (
-    <>
-      <Image src='/school-map/banner.png' className='w-full' width={1440} height={467} alt={''} />
-      <div className='flex flex-col mx-40 sm:mx-0 lg:mx-40'>
-        <Image src='/home/img1.svg' className='w-full pointer-events-none' width={1440} height={467} alt={''} />
-        <Image src='/home/img2.svg' className='w-full pointer-events-none' width={1440} height={467} alt={''} />
-        <Image src='/home/img3.svg' className='w-full py-4 pointer-events-none' width={1440} height={467} alt={''} />
-        {/* <Image src='/home/img4.svg' className='flex flex-row justify-center items-center  py-4 w-60' width={1200} height={48} alt={''} /> */}
-        <Image src='/home/img4.svg' className='w-full py-4 pointer-events-none' width={1440} height={467} alt={''} />
-
-
-
-        <p className='flex flex-row text-5xl text-[#1C355E] my-10 w-1'>RECENT&nbsp;NEWS</p>
-        <hr className={`${styles.solidHr}`} />
-        <RecentNews />
-        </div>
-    </>
+    <div className='bg-grey'>
+      <video
+        src={require("../../public/home.mp4")}
+        autoPlay
+        muted
+        loop
+        width="1903"
+        height="467"
+        className={styles.video}
+      />
+      <div className='bg-[#E5E9EF]'>
+        <Image src='/home/bg.svg' className='w-full pointer-events-none absolute' width={1440} height={467} alt={''} />
+        <main className='flex flex-col mx-40 sm:mx-0 lg:mx-40'>
+          <Image src='/home/img1.svg' className='w-full pointer-events-none z-10 pt-16' width={1440} height={467} alt={''} />
+          <Image src='/home/img5.svg' className='w-full pointer-events-none z-10 pt-16' width={1440} height={467} alt={''} />
+          <Image src='/home/img3.svg' className='w-full py-4 pointer-events-none z-10 pt-32' width={1440} height={467} alt={''} />
+          <Image src='/home/img6.svg' className='w-full py-4 pointer-events-none z-10 pt-32' width={1440} height={467} alt={''} />
+          <Image src='/home/img7.svg' className='w-full py-4 pointer-events-none z-10 pt-32 pb-32' width={1440} height={467} alt={''} />
+          <div className='relative flex items-center'>
+            <div
+              id='slider'
+              className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'
+            >
+              {data.map((item) => (
+                <img
+                  className='w-[688px] h-[852px] inline-block p-0 cursor-pointer hover:scale-105 ease-in-out duration-300'
+                  src={item.img}
+                  alt='/'
+                />
+              ))}
+            </div>
+          </div>
+          <Image src='/home/img4.svg' className='w-full py-4 pointer-events-none z-10' width={1440} height={467} alt={''} />
+          <p className='flex flex-row text-5xl text-[#1C355E] my-10 w-1'>RECENT&nbsp;NEWS</p>
+          <hr className={`${styles.solidHr}`} />
+          <RecentNews />
+        </main>
+      </div>
+    </div>
   )
 }
---
+++ app/[locale]/scholarship/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Page = () => {
+  return (
+    <main className='flex flex-col mx-40 sm:mx-0 lg:mx-40'>
+      <Image src='/sholarship/img1.svg' className='w-full py-4 pointer-events-none z-10 pt-32 pb-32' width={1440} height={467} alt={''} />
+    </main>
+  )
+}
+
+export default Page

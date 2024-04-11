d1ef5de99336083e8345e44d52200866bd56aef5
DESKTOP-FMD3QJU\Ira <kyptdev@gmail.com>
2024-04-11 11:03:24 +0700

    navbar link

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,24 +3,42 @@ import { useTranslations } from 'next-intl'
 import Image from 'next/image'
 import Link from 'next/link'
 import NavbarStyles from './Navbar.module.css'
+import { NavbarLink } from '@/constrant'
 
 const Navbar = () => {
-  const t = useTranslations('Navbar');
+  const t = useTranslations('Navbar')
   return (
     <nav className='bg-secondary flex flex-row relative'>
       <div className='flex flex-row pl-20 h-32'>
       </div>
-      <div className='absolute flex flex-row justify-between w-full px-20 sm:hidden xl:flex'> 
-        <Link href='/en/apply' className={NavbarStyles.menu}>{t('apply')}</Link>
-        <Link href='/book-tour' className={NavbarStyles.menu}>{t('book-tour')}</Link>
-        <Link className={NavbarStyles.menu} href={'/en/school-map'}>{t('school-map')}</Link>
-        {/* <Link className='hover:cursor-pointer' href={'/'}>Home</Link> */}
-        <p className={`${NavbarStyles.menu}`}>{t('contact-us')}</p>
+      <div className='absolute flex flex-row justify-center w-full px-20 sm:hidden xl:flex'>
+        <div className="flex flex-col">
+          <div className='flex flex-row'>
+            <Link href={NavbarLink.apply} className={NavbarStyles.menu}>{t('apply')}</Link>
+            <Link href={NavbarLink.bookATour} target='_blank' className={NavbarStyles.menu}>{t('book-tour')}</Link>
+            <Link className={NavbarStyles.menu} href={'/en/school-map'}>{t('school-map')}</Link>
+            <p className={`${NavbarStyles.menu}`}>{t('contact-us')}</p>
+          </div>
+          <div className='flex flex-row justify-center'>
+            <Link href='#' className={NavbarStyles.menu}>ADMISSIONS</Link>
+            <Link href='#' className={NavbarStyles.menu}>ABOUT RISR</Link>
+            <Link className={NavbarStyles.menu} href={'/en/school-map'}>ACADEMICS</Link>
+          </div>
+        </div>
         <Link href='/'><Image src='/nav.png' width={148} height={200} alt={''} /></Link>
-        <p className={`${NavbarStyles.menu} pl-4`}>{t('news')}</p>
-        <p className={NavbarStyles.menu}>{t('school-calendar')}</p>
-        <p className={NavbarStyles.menu}>{t('work-risr')}</p>
-        <p className={NavbarStyles.menu}>{t('portals')}</p>
+        <div className="flex flex-col">
+          <div className="flex flex-row">
+            <p className={`${NavbarStyles.menu} pl-4`}>{t('news')}</p>
+            <p className={NavbarStyles.menu}>{t('school-calendar')}</p>
+            <p className={NavbarStyles.menu}>{t('work-risr')}</p>
+            <p className={NavbarStyles.menu}>{t('portals')}</p>
+          </div>
+          <div className="flex flex-row justify-center">
+            <p className={`${NavbarStyles.menu} pl-4`}>STUDENT LIFE</p>
+            <p className={NavbarStyles.menu}>COMMUNITY</p>
+            <p className={NavbarStyles.menu}>LINKS</p>
+          </div>
+        </div>
       </div>
       <div className='flex flex-row justify-end pr-20 right-0 absolute'>
         {/* <div className='flex flex-row justify-center'>
---
+++ constrant.tsx
@@ -0,0 +1,6 @@
+const NavbarLink = {
+  apply: '/en/apply',
+  bookATour: 'https://rism.openapply.com/events/new'
+}
+
+export {NavbarLink}

1cc832f6544f8f535f1840fa6b84667a6a208a30
DESKTOP-FMD3QJU\Ira <kyptdev@gmail.com>
2024-04-14 05:43:46 +0700

    wait design edp screen

--- app/[locale]/extended/page.tsx
+++ app/[locale]/extended/page.tsx
@@ -1,6 +1,7 @@
 import Banner from '@/components/Banner'
 import ChildrenLayout from '@/components/ChildrenLayout'
 import SubRichTextNextLine from '@/components/SubRichTextNextLine'
+import { Button } from '@/components/ui/button'
 import { BannerUrl } from '@/constrant'
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
@@ -11,14 +12,39 @@ const Page = () => {
     <>
       <Banner img={BannerUrl.extended} title={t('title')} />
       <ChildrenLayout>
-        {/* <SubRichTextNextLine langKey='Extended.sub2' /> */}
-        <Image
-          src='/extended/content.svg'
-          className='w-full pointer-events-none py-12'
-          width={1440}
-          height={467}
-          alt={''}
-        />
+        <div className='flex flex-row justify-center'>
+          <h1 className='text-[#1C355E] text-[4vw] pt-[3vw]'>{t('title')}</h1>
+        </div>
+        <hr className='border-2 border-solid border-line' />
+        <div className='flex flex-row justify-center mt-[2vw] gap-4'>
+          <Button variant='transblue' className='sub-font px-[6vw]'>{t('regis-form')}</Button>
+          <Button variant='transblue' className='sub-font px-[6vw]'>{t('room-allocation')}</Button>
+          <Button variant='transblue' className='sub-font px-[6vw]'>{t('edp-policy')}</Button>
+        </div>
+        <div className='flex flex-row justify-center'>
+          <h1 className='text-[#1C355E] text-[3vw] pt-[3vw]'>{t('all-courses')}</h1>
+        </div>
+        <div className='flex flex-row justify-center text-center'>
+          <p className='text-[#2E2E2E] text-[0.9vw] pt-[3vw]'>
+            {t.rich('all-courses-sub1', {
+              br: () => <br />
+            })}
+            </p>
+        </div>
+        <div className='flex flex-row justify-center text-center'>
+          <p className='text-[#2E2E2E] text-[0.9vw]'>
+            {t.rich('all-courses-sub2', {
+              b: (c) => <b className='font-bold text-line'>{c}</b>
+            })}
+            </p>
+        </div>
+        <div className='flex flex-row justify-center text-center'>
+          <p className='text-[#2E2E2E] text-[0.9vw]'>
+            {t.rich('all-courses-sub3', {
+              br: () => <br />
+            })}
+            </p>
+        </div>
       </ChildrenLayout>
     </>
   )
